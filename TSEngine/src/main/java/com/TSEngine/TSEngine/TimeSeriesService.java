890c88e635a5625f3840218e310c746eb7d9b2ca
AkshayCharjan <akshaycharjan007@gmail.com>
2025-07-06 13:47:27 +0530

    Added insert and printAll logic

--- TSEngine/src/main/java/com/TSEngine/TSEngine/TimeSeriesService.java
+++ TSEngine/src/main/java/com/TSEngine/TSEngine/TimeSeriesService.java
@@ -1,4 +1,7 @@
 package com.TSEngine.TSEngine;
 
+import java.util.Map;
+
 public interface TimeSeriesService {
+    boolean insert(long timestamp, String metric, double value, Map<String, String> tags);
 }
--- TSEngine/src/main/java/com/TSEngine/TSEngine/TsEngineApplication.java
+++ TSEngine/src/main/java/com/TSEngine/TSEngine/TsEngineApplication.java
@@ -3,11 +3,32 @@ package com.TSEngine.TSEngine;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class TsEngineApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TsEngineApplication.class, args);
+		TimeSeriesServiceImpl timeSeriesServiceImpl = new TimeSeriesServiceImpl();
+
+		long now = System.currentTimeMillis();
+
+		Map<String,String> tags1 = new HashMap<>();
+		tags1.put("host", "server1");
+		tags1.put("dc", "us-west");
+
+		Map<String, String> tags2 = new HashMap<>();
+		tags2.put("host", "server2");
+		tags2.put("dc", "us-east");
+
+		timeSeriesServiceImpl.insert(now,"cpu.usage", 67.5, tags1);
+		timeSeriesServiceImpl.insert(now + 1000, "cpu.usage", 69.2, tags2);
+		timeSeriesServiceImpl.insert(now + 1000, "memory.used", 512.0, tags1);
+
+		timeSeriesServiceImpl.printAllData();
 	}
 
 }
