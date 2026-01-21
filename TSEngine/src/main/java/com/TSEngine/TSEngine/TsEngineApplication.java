1ad2013bf8f1858cc6241ed8bf86d0a9e699fab8
akshaycharjan <akshaycharjan007@gmail.com>
2026-01-21 14:38:27 +0530

    Updated main to use WAL

--- TSEngine/src/main/java/com/TSEngine/TSEngine/TsEngineApplication.java
+++ TSEngine/src/main/java/com/TSEngine/TSEngine/TsEngineApplication.java
@@ -1,8 +1,8 @@
 package com.TSEngine.TSEngine;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,33 +10,46 @@ import java.util.Map;
 @SpringBootApplication
 public class TsEngineApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-//		SpringApplication.run(TsEngineApplication.class, args);
-		TimeSeriesServiceImpl timeSeriesServiceImpl = new TimeSeriesServiceImpl();
+        Path walDir = Path.of("data/wal");
+        WalConfig cfg = WalConfig.defaultDurable(walDir);
 
-		long now = System.currentTimeMillis();
+        TimeSeriesServiceImpl mem = new TimeSeriesServiceImpl();
 
-		Map<String,String> tags1 = new HashMap<>();
-		tags1.put("host", "server1");
-		tags1.put("dc", "us-west");
+        WalReader reader = new WalReader(walDir);
+        reader.replay(mem::replayInsert);
 
-		Map<String, String> tags2 = new HashMap<>();
-		tags2.put("host", "server2");
-		tags2.put("dc", "us-east");
+        WalWriter writer = new WalWriter(cfg);
 
-		timeSeriesServiceImpl.insert(now,"cpu.usage", 67.5, tags1);
-		timeSeriesServiceImpl.insert(now , "cpu.usage", 69.2, tags1);
-		timeSeriesServiceImpl.insert(now, "memory.used", 512.0, tags1);
+        TimeSeriesService service = new TimeSeriesServiceImpl(writer);
 
-		timeSeriesServiceImpl.printAllData();
+        long now = System.currentTimeMillis();
 
-		List<DataPoint> results = timeSeriesServiceImpl.query("cpu.usage", now, now+5000,
-				Map.of("host", "server1"));
+        Map<String, String> tags1 = new HashMap<>();
+        tags1.put("host", "server1");
+        tags1.put("dc", "us-west");
 
-		for (DataPoint dp : results) {
-			System.out.println(dp);
-		}
-	}
+        Map<String, String> tags2 = new HashMap<>();
+        tags2.put("host", "server2");
+        tags2.put("dc", "us-east");
 
+        service.insert(now, "cpu.usage", 67.5, tags1);
+        service.insert(now + 1000, "cpu.usage", 69.2, tags1);
+        service.insert(now + 2000, "memory.used", 512.0, tags2);
+
+        ((TimeSeriesServiceImpl) service).printAllData();
+
+        List<DataPoint> results = service.query(
+                "cpu.usage",
+                now,
+                now + 5000,
+                Map.of("host", "server1")
+        );
+
+        for (DataPoint dp : results) {
+            System.out.println(dp);
+        }
+    }
 }
+
